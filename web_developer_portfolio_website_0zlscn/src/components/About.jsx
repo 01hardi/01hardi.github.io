b0fe9ae2a26bfc4c407bd64c0209bf54f98b759d
Hardiksinh <110206020+01hardi@users.noreply.github.com>
2025-01-17 16:48:03 +0000

    Add files via upload

---
+++ web_developer_portfolio_website_0zlscn/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+    function About() {
+      return (
+        <section id="about">
+          <h2>About Me</h2>
+          <p>Highlight your expertise, career achievements, and goals.</p>
+        </section>
+      );
+    }
+
+    export default About;
